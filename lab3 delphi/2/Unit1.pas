3fb6f4cdb7a8bb0542ee5531b1ea765c00d45546
unknown <anonvtrede@gmail.com>
2021-11-11 23:35:52 +0300

    gavno napisal nado udalit

--- lab3 delphi/2/Unit1.pas
+++ lab3 delphi/2/Unit1.pas
@@ -10,28 +10,55 @@ uses
 
 type
   TForm1 = class(TForm)
+
     PopupMenu1: TPopupMenu;
+
     fsdf1: TMenuItem;
+
     fsdfsd1: TMenuItem;
+
     Panel1: TPanel;
+
     StringGrid1: TStringGrid;
+
     Button1: TButton;
+
     N1: TMenuItem;
+
     Panel2: TPanel;
+
     StringGrid2: TStringGrid;
+
     Button2: TButton;
-    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+
+    N2: TMenuItem;
+
+    procedure FormMouseDown(Sender: TObject;
+
+      Button: TMouseButton;
+
+      Shift: TShiftState;
+
+      X, Y: Integer);
+
     procedure fsdf1Click(Sender: TObject);
+
     procedure FormCreate(Sender: TObject);
+
     procedure Button1Click(Sender: TObject);
+
     procedure fsdfsd1Click(Sender: TObject);
+
     procedure Button2Click(Sender: TObject);
+
     procedure N1Click(Sender: TObject);
+
+    procedure N2Click(Sender: TObject);
+
   private
-    { Private declarations }
+
   public
-    { Public declarations }
+
   end;
 
 var
@@ -45,126 +72,223 @@ const
   n = 4;
 
 type
+  vector = array of Integer;
+
+type
   matrix = array of array of Integer;
-    type vector = array of integer;
+
 var
   mat: matrix;
 
-var
-  i, j: Integer;
-
 function GetDoubledAverage(z: matrix): double;
+
 var
   i, j, sum, counter: Integer;
+
 begin
   for j := 0 to n - 1 do
     for i := 0 to n - 1 do
       if j < i then
+
       begin
         sum := sum + z[i, j];
+
         counter := counter + 1;
+
       end;
+
   result := 2 * (sum / counter);
+
 end;
 
 function GetSumOfPositiveNumberInColumn(a: array of Integer): Integer;
+
 var
   sum, i: Integer;
+
 begin
   for i := 0 to n - 1 do
+
     if a[i] > 0 then
       sum := sum + a[i];
+
   result := sum;
+
 end;
 
 function GetMaxInMatrix(z: matrix): Integer;
+
 var
   i, j, max: Integer;
+
 begin
   max := z[1][1];
+
   for j := 0 to n - 1 do
     for i := 0 to n - 1 do
       if max < z[i][j] then
         max := z[i][j];
 
   result := max;
+
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
+
 begin
   for var j := 0 to n - 1 do
     for var i := 0 to n - 1 do
+
       mat[i, j] := StringGrid1.Cells[i, j].ToInteger();
+
   Panel1.Hide();
+
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
+
 begin
   Panel2.Hide();
+
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
+
 begin
   Panel2.Hide();
+
   Panel1.Hide();
+
   setlength(mat, n);
+
   for var i := 0 to n - 1 do
     setlength(mat[i], n);
 
 end;
 
 procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
+
   Shift: TShiftState; X, Y: Integer);
+
 begin
   var
+
     p: Tpoint;
+
   getcursorpos(p);
+
   if Button = mbRight then
     PopupMenu1.Popup(p.X, p.Y);
 
 end;
 
 procedure TForm1.fsdf1Click(Sender: TObject);
+
 begin
   Panel1.Show();
+
 end;
 
 procedure TForm1.fsdfsd1Click(Sender: TObject);
+
 begin
   Panel2.Show();
+
+  for var j := 0 to n - 1 do
+  begin
+    for var i := 0 to n - 1 do
+
+    begin
+      StringGrid2.Cells[i, j] := mat[i, j].ToString();
+
+    end;
+
+  end;
+
+end;
+
+procedure cpy(var a1, a2: array of Integer; const n: Integer);
+
+var
+  i: Integer;
+
+begin
+  for i := 0 to n - 1 do
+    a1[i] := a2[i];
+
+end;
+
+procedure TForm1.N1Click(Sender: TObject);
+
+var
+  min, r: array of Integer;
+
+var
+
+  i_min: Integer;
+
+var
+  i, j: Integer;
+
+begin
+
+  setlength(min, n);
+
+  setlength(r, n);
+
+  for i := 0 to n - 2 do
+  begin
+    cpy(min, mat[i], n);
+
+    i_min := i;
+
+    for j := i + 1 to n - 1 do
+      if GetSumOfPositiveNumberInColumn(mat[j]) <
+        GetSumOfPositiveNumberInColumn(min) then
+      begin
+
+        cpy(min, mat[j], n);
+
+        i_min := j;
+
+      end;
+
+    cpy(r, mat[i_min], n);
+
+    cpy(mat[i_min], mat[i], n);
+
+    cpy(mat[i], r, n);
+
+  end;
+
+end;
+
+procedure TForm1.N2Click(Sender: TObject);
+
+begin
+  Panel2.Show();
+
+  var
+    temp: vector;
+
+  setlength(temp, n);
+
+  cpy(temp, mat[1], n);
+
+  cpy(mat[1], mat[3], n);
+
+  cpy(mat[3], temp, n);
+
   for var j := 0 to n - 1 do
   begin
     for var i := 0 to n - 1 do
     begin
       StringGrid2.Cells[i, j] := mat[i, j].ToString();
+
     end;
 
   end;
+
 end;
-
-procedure TForm1.N1Click(Sender: TObject);
-var
-  min, r: array of integer;
-var
-  i_min, i: Integer;
-begin
-  if GetMaxInMatrix(mat) < GetDoubledAverage(mat) then
-  begin
-    for i := 0 to n - 2 do
-    begin
-      min := mat[i];
-      i_min := i;
-      for j := i + 1 to n - 1 do
-        if GetSumOfPositiveNumberInColumn(mat[j]) <
-          GetSumOfPositiveNumberInColumn(min) then
-        begin
-          min := mat[j];
-          i_min := j;
-        end;
-      r := mat[i_min];
-      mat[i_min] := mat[i];
-      mat[i] := r;
-    end;
-  end;
 
 end.
---
+++ lab3 delphi/2/__history/Unit1.dfm.~1~
@@ -0,0 +1,91 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnCreate = FormCreate
+  OnMouseDown = FormMouseDown
+  PixelsPerInch = 96
+  TextHeight = 15
+  object Panel1: TPanel
+    Left = 16
+    Top = 8
+    Width = 353
+    Height = 169
+    Caption = 'Panel1'
+    TabOrder = 0
+    object StringGrid1: TStringGrid
+      Left = 40
+      Top = 0
+      Width = 265
+      Height = 105
+      ColCount = 4
+      FixedCols = 0
+      RowCount = 4
+      FixedRows = 0
+      Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goEditing, goFixedRowDefAlign]
+      TabOrder = 0
+    end
+    object Button1: TButton
+      Left = 144
+      Top = 128
+      Width = 75
+      Height = 25
+      Caption = #1042#1074#1077#1089#1090#1080
+      TabOrder = 1
+      OnClick = Button1Click
+    end
+  end
+  object Panel2: TPanel
+    Left = 8
+    Top = 8
+    Width = 361
+    Height = 169
+    Caption = 'Panel2'
+    TabOrder = 1
+    object StringGrid2: TStringGrid
+      Left = 40
+      Top = 0
+      Width = 265
+      Height = 105
+      ColCount = 4
+      FixedCols = 0
+      RowCount = 4
+      FixedRows = 0
+      Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goEditing, goFixedRowDefAlign]
+      TabOrder = 0
+    end
+    object Button2: TButton
+      Left = 136
+      Top = 128
+      Width = 75
+      Height = 25
+      Caption = #1089#1082#1088#1099#1090#1100
+      TabOrder = 1
+      OnClick = Button2Click
+    end
+  end
+  object PopupMenu1: TPopupMenu
+    Left = 24
+    Top = 384
+    object fsdf1: TMenuItem
+      Caption = #1055#1086#1082#1072#1079#1072#1090#1100' '#1086#1082#1085#1086' '#1074#1074#1086#1076#1072' '#1084#1072#1089#1089#1080#1074#1072
+      OnClick = fsdf1Click
+    end
+    object fsdfsd1: TMenuItem
+      Caption = #1042#1099#1074#1077#1089#1090#1080' '#1084#1072#1089#1089#1080#1074
+      OnClick = fsdfsd1Click
+    end
+    object N1: TMenuItem
+      Caption = #1054#1090#1089#1086#1088#1090#1080#1088#1086#1074#1072#1090#1100' '#1084#1072#1089#1089#1080#1074
+      OnClick = N1Click
+    end
+  end
+end
